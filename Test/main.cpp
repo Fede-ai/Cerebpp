2e14d361b7a277a1e652c36247fd6213475ddc12
Federico Lucchi <federico.lucchi07@gmail.com>
2024-02-28 18:22:07 +0100

    shortened lib name

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include "Mlib/Util/util.hpp"
 #include "Mlib/Io/keyboard.hpp"
+#include "Mlib/Util/util.hpp"
 
 int main()
 {
 	while (true)
 	{
 		std::cout << Mlib::Keyboard::getAsyncState(Mlib::Keyboard::A) << '\n';
-		Mlib::sleep(20);
+		Mlib::sleep(10);
 	}
 
 	return 0;
